c8c209b03c0d219f5705e2a2799cefcec4a9171e
SatyamPlayz <redxsatyamyt@gmail.com>
2026-07-09 08:11:52 +0530

    Update NameTagMixin.java

--- src/main/java/com/heroui/client/mixin/NameTagMixin.java
+++ src/main/java/com/heroui/client/mixin/NameTagMixin.java
@@ -1,7 +1,7 @@
 package com.heroui.client.mixin;
 
 import com.heroui.client.HeroUIClient;
-import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class NameTagMixin {
 
     @Inject(method = "hasLabel", at = @At("HEAD"), cancellable = true, remap = true, require = 0)
-    private void heroui$hideNameTag(EntityRenderState state, CallbackInfoReturnable<Boolean> cir) {
+    private void heroui$hideNameTag(LivingEntity entity, double distance, CallbackInfoReturnable<Boolean> cir) {
         if (HeroUIClient.CONFIG != null && HeroUIClient.CONFIG.hideBotNameTags) {
             cir.setReturnValue(false);
         }
