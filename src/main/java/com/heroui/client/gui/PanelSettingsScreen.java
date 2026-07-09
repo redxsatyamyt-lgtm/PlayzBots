bb4be1011213421d186b7c0ea3ec83f37af30cf3
SatyamPlayz <redxsatyamyt@gmail.com>
2026-07-09 08:20:59 +0530

    Update PanelSettingsScreen.java

--- src/main/java/com/heroui/client/gui/PanelSettingsScreen.java
+++ src/main/java/com/heroui/client/gui/PanelSettingsScreen.java
@@ -52,7 +52,6 @@ public class PanelSettingsScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
     }
