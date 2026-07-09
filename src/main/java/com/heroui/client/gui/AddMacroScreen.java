d4474abeb9504388312d988fdc658e87c379d60c
SatyamPlayz <redxsatyamyt@gmail.com>
2026-07-09 08:20:33 +0530

    Update AddMacroScreen.java

--- src/main/java/com/heroui/client/gui/AddMacroScreen.java
+++ src/main/java/com/heroui/client/gui/AddMacroScreen.java
@@ -68,7 +68,6 @@ public class AddMacroScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
         context.drawText(this.textRenderer, "Category", this.width / 2 - 100, 40, 0xAAAAAA, false);
